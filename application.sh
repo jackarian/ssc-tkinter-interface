686b7df369356c0c933841f75660b2320b2709c1
giacomo <ariannagiacomo@gmail.com>
2022-03-19 17:42:10 +0100

    Init project

---
+++ application.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import sys
+import os
+
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(SCRIPT_DIR))
+
+from application import  main
+
+app = main.Application()
+app.master.title('Sample application')
+app.mainloop()
