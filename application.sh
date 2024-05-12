2016203a22d718bcb72ae9efef6cab95a1dc92df
giacomo <ariannagiacomo@gmail.com>
2024-05-12 16:42:09 +0200

    aggiornamento avvio ssc-thinker

--- application.sh
+++ application.sh
@@ -11,12 +11,12 @@ from pathlib import Path
 home = str(Path.home())
 
 from application import main
-from qr_serial import serial_controller
+from camera import controller
 root = Tk()
 app = main.Application(root,
                        "ws://service.local:8080/ssc/prenostazione-risorse/websocket",
                        "/info",
-                        "http://service.local:8080/ssc",1366, 768, serial_controller.QrCodeSerialController)
+                        "http://service.local:8080/ssc",1366, 768, controller.CameraController)
 
 root.title('SSC')
 app.mainloop()
